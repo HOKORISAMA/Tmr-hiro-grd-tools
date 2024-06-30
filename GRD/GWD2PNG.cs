de144a6efcc72eefd2c74fefc9c6a1031d0d0ee0
Hokori <160050298+HOKORISAMA@users.noreply.github.com>
2024-06-30 11:28:22 +0530

    Add files via upload

---
+++ GRD/GWD2PNG.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+class GrdMetaData
+{
+    public ushort Format { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public ushort BPP { get; set; }
+    public int OffsetX { get; set; }
+    public int OffsetY { get; set; }
+    public uint AlphaSize { get; set; }
+    public uint RSize { get; set; }
+    public uint GSize { get; set; }
+    public uint BSize { get; set; }
+}
+
+class GrdReader
+{
+    private BinaryReader _input;
+    private GrdMetaData _info;
+    public byte[] Output { get; private set; }
+    private byte[] _channel;
+    private byte _packType;
+    private int _pixelSize;
+
+    public GrdReader(BinaryReader input, GrdMetaData info)
+    {
+        _input = input;
+        _info = info;
+        Output = new byte[info.Width * info.Height * (info.BPP / 8)];
+        _channel = new byte[info.Width * info.Height];
+        _packType = (byte)(info.Format >> 8);
+        _pixelSize = info.BPP / 8;
+    }
+
+    public void Unpack()
+    {
+        int nextPos = 0x20;
+        if (_info.BPP == 32 && _info.AlphaSize > 0)
+        {
+            UnpackChannel(3, nextPos, (int)_info.AlphaSize);
+            nextPos += (int)_info.AlphaSize;
+        }
+        UnpackChannel(0, nextPos, (int)_info.RSize);
+        nextPos += (int)_info.RSize;
+        UnpackChannel(1, nextPos, (int)_info.GSize);
+        nextPos += (int)_info.GSize;
+        UnpackChannel(2, nextPos, (int)_info.BSize);
+    }
+
+    private void UnpackChannel(int dst, int srcPos, int srcSize)
+    {
+        _input.BaseStream.Seek(srcPos, SeekOrigin.Begin);
+
+        if (_packType == 1)
+        {
+            UnpackRle(_input, srcSize);
+        }
+        else
+        {
+            byte[] data = UnpackHuffman(_input);
+            if (_packType == 0xA2)
+            {
+                UnpackLz77(data, _channel);
+            }
+            else
+            {
+                using (var ms = new MemoryStream(data))
+                using (var br = new BinaryReader(ms))
+                {
+                    UnpackRle(br, data.Length);
+                }
+            }
+        }
+
+        for (int y = _info.Height - 1; y >= 0; y--)
+        {
+            int src = y * _info.Width;
+            for (int x = 0; x < _info.Width; x++)
+            {
+                Output[dst] = _channel[src];
+                src++;
+                dst += _pixelSize;
+            }
+        }
+    }
+
+    private void UnpackRle(BinaryReader inputStream, int srcSize)
+    {
+        int src = 0;
+        int dst = 0;
+        while (src < srcSize)
+        {
+            byte count = inputStream.ReadByte();
+            src++;
+            if (count > 0x7F)
+            {
+                count &= 0x7F;
+                byte v = inputStream.ReadByte();
+                src++;
+                for (int i = 0; i < count; i++)
+                {
+                    _channel[dst++] = v;
+                }
+            }
+            else if (count > 0)
+            {
+                inputStream.Read(_channel, dst, count);
+                src += count;
+                dst += count;
+            }
+        }
+    }
+
+    private static void UnpackLz77(byte[] inputData, byte[] output)
+    {
+        byte special = inputData[8];
+        int src = 12;
+        int dst = 0;
+        while (dst < output.Length)
+        {
+            byte b = inputData[src++];
+            if (b == special)
+            {
+                byte offset = inputData[src++];
+                if (offset != special)
+                {
+                    byte count = inputData[src++];
+                    if (offset > special)
+                    {
+                        offset--;
+                    }
+                    for (int i = 0; i < count; i++)
+                    {
+                        output[dst] = output[dst - offset];
+                        dst++;
+                    }
+                }
+                else
+                {
+                    output[dst++] = offset;
+                }
+            }
+            else
+            {
+                output[dst++] = b;
+            }
+        }
+    }
+
+    private byte[] UnpackHuffman(BinaryReader inputStream)
+    {
+        var tree = CreateHuffmanTree(inputStream);
+        var unpacked = new byte[_huffmanUnpacked];
+        var bits = new LsbBitStream(inputStream);
+        int dst = 0;
+        while (dst < _huffmanUnpacked)
+        {
+            int node = 0x1FE;
+            while (node > 0xFF)
+            {
+                if (bits.GetNextBit() != 0)
+                {
+                    node = tree[node].Right;
+                }
+                else
+                {
+                    node = tree[node].Left;
+                }
+            }
+            unpacked[dst++] = (byte)node;
+        }
+        return unpacked;
+    }
+
+    private int _huffmanUnpacked;
+
+    private HuffmanNode[] CreateHuffmanTree(BinaryReader inputStream)
+    {
+        var nodes = new HuffmanNode[0x200];
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            nodes[i] = new HuffmanNode();
+        }
+        var tree = new List<int>();
+        _huffmanUnpacked = inputStream.ReadInt32();
+        inputStream.ReadInt32(); // packed_size
+
+        for (int i = 0; i < 0x100; i++)
+        {
+            nodes[i].Freq = inputStream.ReadUInt32();
+            AddNode(tree, nodes, i);
+        }
+
+        int lastNode = 0x100;
+        while (tree.Count > 1)
+        {
+            int l = tree[0];
+            tree.RemoveAt(0);
+            int r = tree[0];
+            tree.RemoveAt(0);
+            nodes[lastNode].Freq = nodes[l].Freq + nodes[r].Freq;
+            nodes[lastNode].Left = l;
+            nodes[lastNode].Right = r;
+            AddNode(tree, nodes, lastNode);
+            lastNode++;
+        }
+
+        return nodes;
+    }
+
+    private static void AddNode(List<int> tree, HuffmanNode[] nodes, int index)
+    {
+        uint freq = nodes[index].Freq;
+        for (int i = 0; i < tree.Count; i++)
+        {
+            if (nodes[tree[i]].Freq > freq)
+            {
+                tree.Insert(i, index);
+                return;
+            }
+        }
+        tree.Add(index);
+    }
+}
+
+class HuffmanNode
+{
+    public uint Freq { get; set; }
+    public int Left { get; set; }
+    public int Right { get; set; }
+}
+
+class LsbBitStream
+{
+    private BinaryReader _stream;
+    private byte _currentByte;
+    private int _bitPosition;
+
+    public LsbBitStream(BinaryReader stream)
+    {
+        _stream = stream;
+        _currentByte = 0;
+        _bitPosition = 8;
+    }
+
+    public int GetNextBit()
+    {
+        if (_bitPosition == 8)
+        {
+            _currentByte = _stream.ReadByte();
+            _bitPosition = 0;
+        }
+        int bit = _currentByte & 1;
+        _currentByte >>= 1;
+        _bitPosition++;
+        return bit;
+    }
+}
---
+++ GRD/program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+class Program
+{
+    static GrdMetaData ReadGrdMetadata(string filePath)
+    {
+        using (var fileStream = File.OpenRead(filePath))
+        using (var reader = new BinaryReader(fileStream))
+        {
+            byte[] header = reader.ReadBytes(0x20);
+            if ((header[0] != 1 && header[0] != 2) || (header[1] != 1 && header[1] != 0xA1 && header[1] != 0xA2))
+            {
+                return null;
+            }
+
+            ushort bpp = BitConverter.ToUInt16(header, 6);
+            if (bpp != 24 && bpp != 32)
+            {
+                return null;
+            }
+
+            ushort screenWidth = BitConverter.ToUInt16(header, 2);
+            ushort screenHeight = BitConverter.ToUInt16(header, 4);
+            ushort left = BitConverter.ToUInt16(header, 8);
+            ushort right = BitConverter.ToUInt16(header, 10);
+            ushort top = BitConverter.ToUInt16(header, 12);
+            ushort bottom = BitConverter.ToUInt16(header, 14);
+
+            var info = new GrdMetaData
+            {
+                Format = BitConverter.ToUInt16(header, 0),
+                Width = Math.Abs(right - left),
+                Height = Math.Abs(bottom - top),
+                BPP = bpp,
+                OffsetX = left,
+                OffsetY = screenHeight - bottom,
+                AlphaSize = BitConverter.ToUInt32(header, 0x10),
+                RSize = BitConverter.ToUInt32(header, 0x14),
+                GSize = BitConverter.ToUInt32(header, 0x18),
+                BSize = BitConverter.ToUInt32(header, 0x1C)
+            };
+
+            long fileSize = new FileInfo(filePath).Length;
+            if (0x20 + info.AlphaSize + info.RSize + info.BSize + info.GSize != fileSize)
+            {
+                return null;
+            }
+
+            return info;
+        }
+    }
+
+    static void ConvertGrdToPng(string inputPath, string outputPath)
+    {
+        var info = ReadGrdMetadata(inputPath);
+        if (info == null)
+        {
+            Console.WriteLine($"Invalid GRD file: {inputPath}");
+            return;
+        }
+
+        using (var fileStream = File.OpenRead(inputPath))
+        using (var reader = new BinaryReader(fileStream))
+        {
+            var grdReader = new GrdReader(reader, info);
+            grdReader.Unpack();
+
+            if (info.BPP == 24)
+            {
+                using (var image = Image.LoadPixelData<Rgb24>(grdReader.Output, info.Width, info.Height))
+                {
+                    image.Save(outputPath);
+                }
+            }
+            else if (info.AlphaSize > 0)
+            {
+                using (var image = Image.LoadPixelData<Rgba32>(grdReader.Output, info.Width, info.Height))
+                {
+                    image.Save(outputPath);
+                }
+            }
+            else
+            {
+                using (var image = Image.LoadPixelData<Rgb24>(grdReader.Output, info.Width, info.Height))
+                {
+                    image.Save(outputPath);
+                }
+            }
+        }
+    }
+
+    static void ProcessDirectory(string inputDir, string outputDir)
+    {
+        if (!Directory.Exists(outputDir))
+        {
+            Directory.CreateDirectory(outputDir);
+        }
+
+        foreach (var filePath in Directory.EnumerateFiles(inputDir, "*.grd", SearchOption.AllDirectories))
+        {
+            string relativePath = Path.GetRelativePath(inputDir, filePath);
+            string outputPath = Path.Combine(outputDir, Path.ChangeExtension(relativePath, ".png"));
+            Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+            ConvertGrdToPng(filePath, outputPath);
+            Console.WriteLine($"Converted: {filePath} -> {outputPath}");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Usage: GrdConverter <input> <output>");
+            return;
+        }
+
+        string input = args[0];
+        string output = args[1];
+
+        if (Directory.Exists(input))
+        {
+            ProcessDirectory(input, output);
+        }
+        else
+        {
+            ConvertGrdToPng(input, output);
+        }
+    }
+}
